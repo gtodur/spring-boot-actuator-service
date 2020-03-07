8fdb391467599313c828d98b191c1953b190573b
gtodur <gtodur@outlook.com>
2020-03-07 10:19:12 +0530

    first commit

---
+++ src/main/java/com/df/springboot/actuator/service/SpringBootActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.df.springboot.actuator.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorServiceApplication.class, args);
+	}
+
+}
